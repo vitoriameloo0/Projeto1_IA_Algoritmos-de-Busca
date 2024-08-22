d2b278839703215b525463f22dd2cd48ab80f5db
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2024-08-22 12:37:51 -0300

    transferindoFuncao

---
+++ pioresSolucoes.py
@@ -0,0 +1,20 @@
+# Funcao para as opcoes de piores solucoes
+def piorSolucao():
+    opcao = 0
+    while opcao != 3:
+        print('\nAlgoritmos de Piores Soluções: ')
+        print('1 - Busca em Profundidade')
+        print('2 - Busca em Largura')
+        print('3 - Voltar para o Menu de Opções')
+
+        opcao = int(input('Escolha uma opção: '))
+        if opcao == 1:
+            print('Falta Implementar')
+        elif opcao == 2:
+            print('Falta Implementar')
+        elif opcao == 3:
+            print('Voltando...')
+        else:
+            print('Opcao Invalida. Tente novamente')
+        print('=-='*10)
+    print('\n'*2)
