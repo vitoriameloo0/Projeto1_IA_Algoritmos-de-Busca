86277f0ec4fb8cacaba2f8fa59c6bd68899a0df6
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2024-08-22 12:37:37 -0300

    transferindoFuncao

---
+++ melhoresSolucoes.py
@@ -0,0 +1,20 @@
+# Funcao de menu para as opcoes de melhores solucoes
+def melhorSolucao():
+    opcao = 0
+    while opcao != 3:
+        print('\nAlgoritmos de Melhores Soluções: ')
+        print('1 - Busca A*')
+        print('2 - Busca de Custo Uniforme')
+        print('3 - Voltar para o Menu de Opções')
+
+        opcao = int(input('Escolha uma opção: '))
+        if opcao == 1:
+            print('Falta Implementar')
+        elif opcao == 2:
+            print('Falta Implementar')
+        elif opcao == 3:
+            print('Voltando...')
+        else:
+            print('Opcao Invalida. Tente novamente')
+        print('=-='*10)
+    print('\n'*2)
